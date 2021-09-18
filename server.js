36de3000d9066faaa531bfb1342b37185d3b11cc
trankhoa2810 <trankhoa2810@gmail.com>
2021-09-18 15:43:56 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError } = require("./app/helpers/errors");
+const { response } = require("express");
 
 const app = express();
 
@@ -20,6 +22,17 @@ app.get("/", (req, res) => {
 });
 
 setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+        mesage: err.mesage || "Internal Sever Error",
+    });
+});
 
 const PORT = config.app.port;
 app.listen(PORT, () => {
